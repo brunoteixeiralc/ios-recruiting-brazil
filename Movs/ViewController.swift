b675e16b9aa33f3b92342b6e46aceaa3bd88ba9d
Bruno Teixeira Lemgruber Corrêa <bruno.correa@ofornecedores.sicoob.com.br>
2018-10-04 17:17:33 -0300

    Initial Commit

---
+++ Movs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movs
+//
+//  Created by Bruno Lemgruber on 04/10/18.
+//  Copyright © 2018 Bruno Correa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
