929d6d1283ee0f31269aff0c51710b98fb8fc742
Bruno Teixeira Lemgruber Corrêa <bruno.correa@ofornecedores.sicoob.com.br>
2018-10-05 18:07:41 -0300

    [commit] - add storyboard sequence

---
+++ Movs/ViewControllers/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  Movs
+//
+//  Created by Bruno Lemgruber on 05/10/18.
+//  Copyright © 2018 Bruno Correa. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
